f12a6043c3af250ec3142e84ff4e426e2b7ff4cb
gav1306 <gavk30@gmail.com>
2023-09-30 19:36:32 +0530

    first commit

---
+++ infoScript.js
@@ -0,0 +1,53 @@
+const tempEl = document.getElementById("temp");
+const fahrenheitCheckEl = document.getElementById("fahrenheitCheck");
+const tempSymbolEl = document.getElementById("tempSymbol");
+const temperatureEl = document.getElementById("temperature");
+const minTempEl = document.getElementById("minTemp");
+const maxTempEl = document.getElementById("maxTemp");
+const windEl = document.getElementById("wind");
+const windSpeedEl = document.getElementById("windSpeed");
+const feelsLikeEl = document.getElementById("feelsLike");
+const windDegreeEl = document.getElementById("windDegree");
+const humidityEl = document.getElementById("humidity");
+const humidity2El = document.getElementById("humidity2");
+const sunriseEl = document.getElementById("sunrise");
+const sunsetEl = document.getElementById("sunset");
+
+const weatherData = JSON.parse(localStorage.getItem("weatherData"));
+
+if (!weatherData) {
+  window.location.href = "http://127.0.0.1:5500/index.html";
+}
+
+const celsiusToFahrenheit = (celsius) => {
+  return (celsius * 9) / 5 + 32;
+};
+
+const changeTempType = (e) => {
+  if (e.target.checked) {
+    tempEl.textContent = celsiusToFahrenheit(weatherData.temp);
+    tempSymbolEl.innerHTML = "&#x2109;";
+    return;
+  }
+  tempEl.textContent = weatherData.temp;
+  tempSymbolEl.innerHTML = "&#8451;";
+};
+
+const setData = () => {
+  tempEl.textContent = weatherData.temp;
+  temperatureEl.textContent = weatherData.temp;
+  minTempEl.textContent = weatherData.min_temp;
+  maxTempEl.textContent = weatherData.max_temp;
+  windEl.textContent = weatherData.wind_speed;
+  windSpeedEl.textContent = weatherData.wind_speed;
+  feelsLikeEl.textContent = weatherData.feels_like;
+  windDegreeEl.textContent = weatherData.wind_degrees;
+  humidityEl.textContent = weatherData.humidity;
+  humidity2El.textContent = weatherData.humidity;
+  sunriseEl.textContent = weatherData.sunrise;
+  sunsetEl.textContent = weatherData.sunset;
+};
+
+fahrenheitCheckEl.addEventListener("change", changeTempType);
+
+setData();
---
+++ script.js
@@ -0,0 +1,63 @@
+const inputEl = document.getElementById("searchInput");
+const submitBtn = document.getElementById("submitBtn");
+const errorEl = document.getElementById("error");
+
+//variables
+let userInput = "";
+const weatherUrl = "https://weather-by-api-ninjas.p.rapidapi.com/v1/weather";
+const apiKey = "54055a5eafmsh4468d9cbc165f03p1d3892jsn062d952dc838";
+
+//external request
+const fetchWeather = () => {
+  const isZip = !isNaN(+userInput);
+  fetch(`${weatherUrl}?${isZip ? `zip=${userInput}` : `city=${userInput}`}`, {
+    headers: {
+      "X-RapidAPI-Key": apiKey,
+    },
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Something went wrong");
+      }
+
+      return response.json();
+    })
+    .then((data) => {
+      localStorage.setItem("weatherData", JSON.stringify(data));
+      inputEl.value = "";
+      window.location.href = "http://127.0.0.1:5500/info.html";
+    })
+    .catch((error) => {
+      errorEl.style.display = "block";
+      errorEl.textContent = error.message;
+      setTimeout(() => {
+        closeErr();
+      }, 2000);
+    });
+};
+
+//functions
+const searchInfo = (e) => {
+  userInput = e.target.value;
+};
+
+const getWeather = () => {
+  if (!userInput) {
+    errorEl.style.display = "block";
+    errorEl.textContent = "Please enter a City name or Zip Code!";
+    setTimeout(() => {
+      closeErr();
+    }, 2000);
+    return;
+  }
+  fetchWeather();
+};
+
+const closeErr = () => {
+  errorEl.style.display = "none";
+  errorEl.textContent = "";
+};
+
+//event listeners
+inputEl.addEventListener("change", searchInfo);
+submitBtn.addEventListener("click", getWeather);
